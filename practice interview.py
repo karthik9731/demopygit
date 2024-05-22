ec808c858c0ae8f887176b75714a8dfb21b17de7
Karthik <ckarthikc1912@gmail.com>
2024-05-22 16:12:30 +0530

    2 programs added

---
+++ practice interview.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+# remove duplicates
+
+# l=[1,2,5,4,5,7,8,4,5,7,8,1,2,4,5,1,7,8]
+# dup = []
+# for i in l:
+#     if i not in dup:
+#         dup.append(i)
+# print(dup)
+
+# Common elements
+
+l=[1,2,5,4,5,7,8,4,5,7,8,1,2,4,5,1,7,8]
+l1=[7,8,1,4,6,3,2,1,5,9,6,3,1,4,3]
+def common_ele(list,list1):
+    common =[]
+    for i in list:
+        if i not in list1:
+            common.append(i)
+    return common
+print(common_ele(l,l1))
